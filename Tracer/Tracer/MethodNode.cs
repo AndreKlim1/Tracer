378a3b4b753788789eaecc66404c6ec6eadaf9ef
AndreKlim1 <andreyklimovich159@gmail.com>
2024-04-06 17:30:31 +0300

    MethodNode completed, Tracer started

--- Tracer/Tracer/MethodNode.cs
+++ Tracer/Tracer/MethodNode.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Tracer.Entities;
@@ -13,6 +13,58 @@ namespace Tracer.Tracer
 {
     public class MethodNode
     {
+        private MethodStruct _methodStruct;
+        private Stopwatch _stopwatch;
 
+        [JsonProperty(PropertyName = "method properties")]
+        public MethodStruct GetMethodStruct { get { return _methodStruct; } }
+
+        
+        [JsonIgnore]
+        [XmlIgnore]
+        public MethodNode parentMethod;
+
+        private (string, string) GetCallingMethodNameAndClassName()
+        {
+            StackTrace stackTrace = new StackTrace();
+            StackFrame[] stackFrames = stackTrace.GetFrames();
+
+            int skipFrames = 2;
+
+
+            for (int i = skipFrames; i < stackFrames.Length; i++)
+            {
+                MethodBase method = stackFrames[i].GetMethod();
+                if (method.DeclaringType != typeof(Tracer))
+                {
+                    return (method.Name, method.DeclaringType.Name);
+                }
+            }
+
+            return (string.Empty, string.Empty);
+        }
+
+
+        public void StartStopwatch()
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void StopStopwatch()
+        {
+            TimeSpan elapsedTime = _stopwatch.Elapsed;
+            _methodStruct.Time = elapsedTime.TotalMilliseconds;
+        }
+
+        public MethodNode()
+        {
+            _methodStruct.internalMethodStructs = new List<MethodNode>();
+            StackTrace stackTrace = new StackTrace();
+            (string, string) res = GetCallingMethodNameAndClassName();
+
+            _methodStruct.Name = res.Item1;
+            _methodStruct.ClassName = res.Item2;
+            _methodStruct.MethodDepth = stackTrace.FrameCount - 4;
+        }
     }
 }
--- Tracer/Tracer/Tracer.cs
+++ Tracer/Tracer/Tracer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,27 @@ namespace Tracer.Tracer
 {
     public class Tracer : ITracer
     {
+        private TraceResultStruct _traceResultStruct;
+        private MethodNode _method;
+        private MethodNode _prevMethod;
+        private int _currentMethodDepth;
+        private static ConcurrentDictionary<int, TraceResultStruct> _tracersDict;
+
         public TraceResultStruct GetTraceResult()
         {
-            throw new NotImplementedException();
+            CountTotalTime();
+
+            _tracersDict.AddOrUpdate(_traceResultStruct.Id, _traceResultStruct, (key, existingValue) => _traceResultStruct);
+            return _traceResultStruct;
+        }
+
+        private void CountTotalTime()
+        {
+            _traceResultStruct.Time = 0;
+            foreach (var method in _traceResultStruct.Methods)
+            {
+                _traceResultStruct.Time += method.GetMethodStruct.Time;
+            }
         }
 
         public void StartTrace()
@@ -21,7 +40,39 @@ namespace Tracer.Tracer
 
         public void StopTrace()
         {
-            throw new NotImplementedException();
+            _method.StopStopwatch();
+
+
+
+            if (_prevMethod == _method)
+            {
+                if (_prevMethod.parentMethod != null)
+                {
+                    _method = _prevMethod.parentMethod;
+                }
+            }
+            else
+            {
+                if (_method.GetMethodStruct.MethodDepth == 0)
+                {
+                    _prevMethod = _method;
+                }
+                else
+                {
+                    _method = _prevMethod;
+                }
+            }
+
+        }
+
+        public Tracer(int threadID)
+        {
+            _traceResultStruct.Id = threadID;
+            _traceResultStruct.Methods = new List<MethodNode>();
+            if (_tracersDict == null)
+            {
+                _tracersDict = new ConcurrentDictionary<int, TraceResultStruct>();
+            }
         }
     }
 }
